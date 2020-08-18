a97eb4cb8a19f73d8d820e9eff94031476efe962
19011646 <19011646@DESKTOP-AMHMBRS>
2020-08-18 10:03:02 +0800

    Added add customer, view customer and delete customer. Kun Xuan

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,28 @@
-
+import java.util.ArrayList;
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//ArrayList for customer's name
+		ArrayList<String> customer = new ArrayList<String>();
+		
+		//add customer's name
+        String name = Helper.readString("Enter a customer name > ");
+        customer.add(name);
+        
+        //view customer 
+        for (int i = 0; i < customer.size(); i++) {
+        	System.out.println(name);
+        }
+        
+        //delete customer name
+        String dname = Helper.readString("Enter a customer name to delete > ");
+        for (int s = 0; s < customer.size(); s++) {
+        	if (dname.equals(name)) {
+        		customer.remove(s);
+        	}
+        }
+        
 	}
 
 }
