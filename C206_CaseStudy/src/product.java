70228b2c3112be11207ab4fd991a046a07f99bb2
19015428 <19015428@LAPTOP-734V9TF0>
2020-08-19 20:04:34 +0800

    addProduct, viewProduct, deleteProduct

---
+++ C206_CaseStudy/src/product.java
@@ -0,0 +1,33 @@
+
+	public class product {
+		
+		private String productName;
+		private String category;
+		private double price;
+		
+		public product(String productName, String category, double price) {
+			this.productName = productName;
+			this.category = category;
+			this.price = price;
+		}
+		
+		public String addProduct() {
+			return productName;
+		}
+		
+		public String viewProduct() {
+			return productName;
+		}
+		
+		public String deleteProduct() {
+			return productName;
+		}
+		
+		public String viewCategory() {
+			return category;
+		}
+		
+		public double setPrice() {
+			return price;
+		}
+	}
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+import java.util.ArrayList;
 
 import org.junit.After;
 import org.junit.Before;
@@ -18,6 +19,17 @@ public class C206_CaseStudyTest {
 	public void c206_test() {
 		//fail("Not yet implemented"); 
 		assertTrue("C206_CaseStudy_SampleTest ",true);
+	}
+	
+	public static void main(String[] args) {
+		
+		ArrayList<product> categoryList = new ArrayList<product>();
+		String category = Helper.readString("Enter a category to view > ");
+		ArrayList<product> productList = new ArrayList<product>();
+		String product = Helper.readString("enter product to add > ");
+		
+		String view = Helper.readString("enter a product to view > ");
+		
 	}
 
 }
